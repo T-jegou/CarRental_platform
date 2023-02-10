d9b00853bc930b68dfa579090ad1a51582f24191
T-jegou <thomas.jegou1@gmail.com>
2023-02-10 14:45:08 +0100

    Fix tfe set env retrieve

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "ekipDevops"
@@ -7,17 +14,17 @@ terraform {
       prefix = "CarRental-"
     }
   }
+}
 
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+provider "aws" {
+  region     = "eu-west-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 variable "app_name" {
   type    = string
-  default = "WebApache"
+  default = "CarPlatform"
 }
 
 # VPC
