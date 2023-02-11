e60eabe83b998b6ed0104f227c1d9f04db47050e
T-jegou <thomas.jegou1@gmail.com>
2023-02-11 14:17:11 +0100

    Update : Infrastructe improvement , discover az and create on each az a priv and pub sub

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -27,16 +27,34 @@ variable "app_name" {
   default = "CarPlatform"
 }
 
-# VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  instance_tenancy     = "default"
+data "aws_availability_zones" "available" {}
 
+resource "aws_vpc" "CarPlatform-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
   tags = {
-    Name    = "${terraform.workspace}-vpc"
-    Project = "${var.app_name}"
+    Name = "${terraform.workspace}-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(data.aws_availability_zones.available.names)
+  vpc_id                  = aws_vpc.CarPlatform-vpc.id
+  cidr_block              = "10.0.${10 + count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${terraform.workspace}-PublicSubnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count                   = length(data.aws_availability_zones.available.names)
+  vpc_id                  = aws_vpc.CarPlatform-vpc.id
+  cidr_block              = "10.0.${20 + count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${terraform.workspace}-PrivateSubnet"
   }
 }
